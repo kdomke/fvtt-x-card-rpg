9dd7b39c583e9166832b26be4324cb121d2e9dd5
Kristian Domke <kristian.domke@nassmagnet.de>
2025-08-14 10:16:54 +0200

    Adds debugging (2)

--- src/js/x-card-rpg/ui.js
+++ src/js/x-card-rpg/ui.js
@@ -15,18 +15,17 @@ export default class Ui {
      * @private
      */
     _add_x_card_button() {
-        Hooks.on('getSceneControlButtons', buttonsList =>{
-
-            buttonsList.array.forEach(menuButton => {
-                menuButton.xCardRPG = {
-                        name: "XCardRpg.ButtonName",
-                        title: "XCardRpg.ButtonTitle",
-                        icon: "fas fa-times",
-                        button: true,
-                        onClick : this.click_handler.bind(this),
+        Hooks.on('getSceneControlButtons', buttonsList)
+        console.log("x-card-rpg hook called")
+        buttonsList.array.forEach(menuButton => {
+            menuButton.xCardRPG = {
+                    name: "XCardRpg.ButtonName",
+                    title: "XCardRpg.ButtonTitle",
+                    icon: "fas fa-times",
+                    button: true,
+                    onClick : this.click_handler.bind(this),
                 }
-            });
-        })
+        });
     }
 
     /**
